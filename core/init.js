f7ff4a3a8a0910777ec80cf822635c5bc0a5d6c8
cmarrero01 <cmarrero01@gmail.com>
2014-05-13 14:58:48 -0300

    Mini platform game without real time actions, this is an example of Quintus Framework

--- core/init.js
+++ core/init.js
@@ -1,3 +1,27 @@
 /**
- * Created by cmarrero on 13/05/14.
+ * Server of RealTimePlatformGame main file.
+ * Every start here
+ * @module Server
+ * @author Claudio Marrero
+ * @copyright 2014 Claudio A. Marrero
  */
+
+module.exports = function(io){
+
+	var Game = (function(){
+
+		function setup(socket){
+
+		}
+
+		function listener(socket){
+
+		}
+
+		return {
+			setup:setup
+		}
+	})();
+
+	io.sockets.on('connection', Game.setup);
+};
